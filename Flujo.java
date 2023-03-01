bd5597d2b9d64854528efa15222a7076242745b8
Cerecero <ale.rey.cer@outlook.com>
2023-03-01 11:47:51 -0600

    added a new class called Flujo

---
+++ Flujo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java.pila.ejecuccion;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Flujo {
+
+        public static void main(String[] args) {
+            System.out.println("Inicio del main");
+            metodo1();
+            System.out.println("Fin del main");
+        }
+
+        private static void metodo1() {
+            System.out.println("Inicio del metodo1");
+            metodo2();
+            System.out.println("Fin del metodo1");
+        }
+
+        private static void metodo2() {
+            System.out.println("Inicio del metodo2");
+            for(int i = 1; i <= 5; i++) {
+                System.out.println(i);
+            }
+            System.out.println("Fin del metodo2");
+        }
+
+}
